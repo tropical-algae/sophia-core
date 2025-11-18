485d0a798e70e836e5aad04d1a5bc7a1a0769af1
tropicalalgae <tropicalalgae@gmail.com>
2025-11-19 02:47:57 +0800

    chore: Supplement test cases for new api

---
+++ tests/api/test_agent.py
@@ -0,0 +1,35 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from sophia.common.config import settings
+from sophia.core.model.base import SystemStatusType
+from sophia.core.model.user import ScopeType
+from tests.conftest import DataStore
+
+FAKE_SESION_ID: str = "FAKEID"
+
+
+@pytest.mark.run(order=5)
+def test_model_check_api(client: TestClient, data_store: DataStore):
+    header = {"Authorization": f"Bearer {data_store.admin_token_data}"}
+    url = f"{settings.API_PREFIX}/session/list"
+    response = client.get(url=url, headers=header)
+    assert response.status_code == 200
+
+
+@pytest.mark.run(order=6)
+def test_session_check_api(client: TestClient):
+    url = f"{settings.API_PREFIX}/chat/models"
+    response = client.get(url=url)
+    assert response.status_code == 200
+
+
+@pytest.mark.run(order=7)
+def test_message_check_api(client: TestClient, data_store: DataStore):
+    header = {
+        "Authorization": f"Bearer {data_store.admin_token_data}",
+        "X-Session-Id": FAKE_SESION_ID,
+    }
+    url = f"{settings.API_PREFIX}/session/messages"
+    response = client.post(url=url, headers=header)
+    assert response.status_code == 404
